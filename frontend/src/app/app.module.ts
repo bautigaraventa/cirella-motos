f92058712083e14ec1c024fba7cf48916ef3cea4
bautigaraventa <bautistagaraventa@hotmail.com>
2018-05-27 20:17:58 -0300

    not understanding the usage of bootstrap

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppBootstrapModule } from './app-bootstrap/app-bootstrap.module';
+
 import { AppComponent } from './app.component';
 import { MotosComponent } from './motos/motos.component';
 import { MotoThumbnailComponent } from './moto-thumbnail/moto-thumbnail.component';
@@ -13,6 +15,7 @@ import { MotoThumbnailComponent } from './moto-thumbnail/moto-thumbnail.componen
   ],
   imports: [
     BrowserModule,
+    AppBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
